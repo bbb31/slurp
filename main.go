5d4a590bef85beeba6b69c56279ea40e7ad8826f
sweetroll <sweetrollbandit@protonmail.com>
2018-08-27 12:39:40 -0600

    Updated shame-code

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import ("fmt"
+	"time"
+	)
 
 func main() {
+	fmt.Println("Executing malware...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Downloading all your files...")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("Activating ransomware...")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("Mining bitcoin...")
+	time.Sleep(2 * time.Second)
+	fmt.Println("Just kidding")
+	time.Sleep(500 * time.Millisecond)
 	fmt.Println("You could have been hacked! Don't blindly run code off Github just because someone you trust linked it to you.")
 }
